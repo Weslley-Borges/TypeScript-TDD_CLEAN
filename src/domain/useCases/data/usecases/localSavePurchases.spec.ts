ee2ab10e01523e576539e2410dcb173f4df13510
Weslley Borges <weslley15bs@gmail.com>
2021-08-04 19:17:31 -0300

    FEAT: Ensure LocalSavePurchases does not delete cache upon creation

---
+++ src/domain/useCases/data/usecases/localSavePurchases.spec.ts
@@ -0,0 +1,19 @@
+class LocalSavePurchases {
+  constructor(
+    private readonly cacheStore:CacheStore
+  ) {}
+}
+
+interface CacheStore {}
+
+class CacheStoreSpy implements CacheStore {
+  deleteCallsCount = 0
+}
+
+describe('LocalSavePurchases', () => {
+  test('Should not delete cache on suit.init', () => {
+    const cacheStore = new CacheStoreSpy
+    const sut = new LocalSavePurchases(cacheStore)
+    expect(cacheStore.deleteCallsCount).toBe(0)
+  })
+})
