58e23a52d1b61f92174f3ae88b53ca081f59ec8b
Weslley Borges <weslley15bs@gmail.com>
2021-08-04 19:32:38 -0300

    FEAT: Move SUT creation to a factory helper method

--- src/domain/useCases/data/usecases/localSavePurchases.spec.ts
+++ src/domain/useCases/data/usecases/localSavePurchases.spec.ts
@@ -20,16 +20,30 @@ class CacheStoreSpy implements CacheStore {
   }
 }
 
+type SutTypes = {
+  sut: LocalSavePurchases,
+  cacheStore: CacheStoreSpy
+}
+
+const makeSut = (): SutTypes => {
+  const cacheStore = new CacheStoreSpy
+  const sut = new LocalSavePurchases(cacheStore)
+  return {
+    sut,
+    cacheStore
+  }
+}
+
+
 describe('LocalSavePurchases', () => {
   test('Should not delete cache on suit.init', () => {
-    const cacheStore = new CacheStoreSpy
+    const { cacheStore } = makeSut()
     new LocalSavePurchases(cacheStore)
     expect(cacheStore.deleteCallsCount).toBe(0)
   })
 
   test('Should delete old cache on suit.save', async () => {
-    const cacheStore = new CacheStoreSpy
-    const sut = new LocalSavePurchases(cacheStore)
+    const { sut, cacheStore } = makeSut()
     await sut.save()
     expect(cacheStore.deleteCallsCount).toBe(1)
   })
